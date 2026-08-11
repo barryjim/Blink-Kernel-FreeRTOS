9a1e053ab02acb0aae876152b7f7ec031e748b82
barryjim <461746775@qq.com>
2026-08-11 15:44:57 +0800

    v22

--- config/version.h
+++ config/version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define SOFTWARE_VERSION       "1.0.1"
+#define SOFTWARE_VERSION       "1.0.10"
 #define SOFTWARE_VERSION_MAJOR 1
 #define SOFTWARE_VERSION_MINOR 0
-#define SOFTWARE_VERSION_PATCH 1
+#define SOFTWARE_VERSION_PATCH 10
 
 #endif
